f0a7aeeb9440a953f6d433ee655bc6651ac33f8e
kenjitheman <kenjith3man@gmail.com>
2023-11-20 21:48:11 +0200

    chore(format): Apply new code formatting style (set.lua) Closes #11

--- lua/kenjitheman/set.lua
+++ lua/kenjitheman/set.lua
@@ -6,9 +6,9 @@ vim.opt.number = true
 vim.opt.relativenumber = true
 
 -- Set tab-related options
-vim.opt.tabstop = 2
-vim.opt.softtabstop = 2
-vim.opt.shiftwidth = 2
+vim.opt.tabstop = 4
+vim.opt.softtabstop = 4
+vim.opt.shiftwidth = 4
 vim.opt.expandtab = true
 
 -- Enable smart indent
