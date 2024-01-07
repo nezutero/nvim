133059c6c4d5292ac4804f0716e9428c7ef137ce
btwkenji <btw4dev@gmail.com>
2024-01-07 13:35:21 +0200

    chore: format code

--- after/plugin/lsp.lua
+++ after/plugin/lsp.lua
@@ -16,7 +16,6 @@ lsp.ensure_installed({
 -- Fix Undefined global 'vim'
 lsp.nvim_workspace()
 
-
 local cmp = require('cmp')
 local cmp_select = { behavior = cmp.SelectBehavior.Select }
 local cmp_mappings = lsp.defaults.cmp_mappings({
