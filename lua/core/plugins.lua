db7492e31ed5ee1665ee1f7ae05c9008c4e9d5cf
nezutero <nezutero@pm.me>
2025-03-09 20:13:59 +0100

    feat: add ibl

--- lua/core/plugins.lua
+++ lua/core/plugins.lua
@@ -99,6 +99,13 @@ require("lazy").setup({
     "linrongbin16/lsp-progress.nvim",
     event = { "VimEnter" },
     dependencies = { "nvim-tree/nvim-web-devicons" },
+    {
+        "lukas-reineke/indent-blankline.nvim",
+        main = "ibl",
+        ---@module "ibl"
+        ---@type ibl.config
+        opts = {},
+    },
     config = function() require("lsp-progress").setup() end
 },
     {
@@ -106,18 +113,4 @@ require("lazy").setup({
         config = true,
         cmd = "Glow"
     },
-    {
-        "folke/flash.nvim",
-        event = "VeryLazy",
-        ---@type Flash.Config
-        opts = {},
-        -- stylua: ignore
-        keys = {
-            { "s",     mode = { "n", "x", "o" }, function() require("flash").jump() end,              desc = "Flash" },
-            { "S",     mode = { "n", "x", "o" }, function() require("flash").treesitter() end,        desc = "Flash Treesitter" },
-            { "r",     mode = "o",               function() require("flash").remote() end,            desc = "Remote Flash" },
-            { "R",     mode = { "o", "x" },      function() require("flash").treesitter_search() end, desc = "Treesitter Search" },
-            { "<c-s>", mode = { "c" },           function() require("flash").toggle() end,            desc = "Toggle Flash Search" },
-        },
-    }
 });
